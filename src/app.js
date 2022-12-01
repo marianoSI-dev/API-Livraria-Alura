fccc03067a0dfb188e71780e601a26d3d0f2a11a
Mariano Silva <mariano.silva@dtidigital.com>
2022-11-30 22:42:06 -0300

    Get e post no padrão mvc

--- src/app.js
+++ src/app.js
@@ -1,27 +1,31 @@
 import express from "express";
+import db from '../config/dbConnect.js';
+import livros from '../models/Livro.js';
+import routes from '../Routes/index.js'
+
+db.on("error", console.log.bind(console, "Erro de conexão"))
+db.once("open", ()=> {
+    console.log("conexão com o banco feita com sucesso")
+})
 
 const app = express();
 app.use(express.json());
+routes(app)
 
-const livros= [{id:1, "titulo": "O Senhor dos Anéis"}, {id:2, "titulo":"O Hobbit"}];
+//const livros= [{id:1, "titulo": "O Senhor dos Anéis"}, {id:2, "titulo":"O Hobbit"}];
 
-app.get('/',(req,res)=>{
-    res.status(200).send("Curso de NODE Alura");
-})
+
+
+
+
 app.get("/livros/:id", (req,res)=>{
     const index = buscaLivro(req.params.id);
     res.status(200).json(livros[index])
 
 })
 
-app.get("/livros", (req,res)=>{
-    res.status(200).json(livros)
-});
 
-app.post("/livros", (req,res)=>{
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso");
-})
+
 
 app.put("/livros/:id", (req,res)=>{
     const index = buscaLivro(req.params.id);
